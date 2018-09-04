ee9b04fb325733949cb3dde1dda25d9e57f058ab
narek.pedanyan <narek.pedanyan@betconstruct.com>
2018-09-04 18:40:16 +0400

    My second commit

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <h1>My asdasd App!</h1>
+                <h1>You can hate me now!</h1>
             </div>
         );
     }
