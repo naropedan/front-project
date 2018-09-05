cfb397271e330e19d4bfe754ed41c20b5ec18065
narek.pedanyan <narek.pedanyan@betconstruct.com>
2018-09-05 19:07:29 +0400

    My second Redux add

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <h1>You can hate me now!</h1>
+                <h1>You can hasdasasdsadate me now!</h1>
             </div>
         );
     }
